0d1dab56766d6d59d0c25a2fc3cab2daaf08ea86
zhanghr <429763991@qq.com>
2015-11-11 15:39:40 +0800

    [update]

--- src/main/java/com/swim/redis/Test.java
+++ src/main/java/com/swim/redis/Test.java
@@ -1,9 +1,16 @@
 package com.swim.redis;
 
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    Settings settings = ImmutableSettings.settingsBuilder().put("client.transport.sniff", true).put("cluster.name", "name of node").build();  
+	    Client client = new TransportClient(settings).addTransportAddress(new InetSocketTransportAddress("ip of server", 9300));  
 
 	}
 
